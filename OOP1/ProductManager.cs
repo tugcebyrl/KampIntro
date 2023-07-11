8ebf1a280c170ef2238c571c76f84231a6cc3d36
Tugce BAYIRLI <tbayirli@hotmail.com>
2023-07-11 21:53:56 +0300

    OOP

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    internal class ProductManager
+    {
+        //Encapsulation
+        public void Add(Product product)
+        {
+            //product.ProductName = "Kamera";
+
+            Console.WriteLine(product.ProductName + "Eklendi.");
+
+        }
+
+        public void Update(Product product)
+        {
+            //product.ProductName = "Kamera";
+
+            Console.WriteLine(product.ProductName + "Güncellendi.");
+
+        }
+
+        public int Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+
+        public void Topla2(int sayi1, int sayi2)
+        {
+            Console.WriteLine(sayi1 + sayi2); 
+        }
+        //public void BirSeyYap(int sayi)
+        //{
+        //sayi = 99;
+        //}
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.InteropServices;
+
+namespace OOP1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Tanımlama Sekli
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+            // 2. Tanımlama Sekli
+
+            Product product2 = new Product {Id = 2, CategoryId = 5, ProductName = "Kalem", UnitPrice=50, UnitsInStock=100};
+            
+            //case sensitive
+            //PascalCase    //camelCase
+            ProductManager productManager = new ProductManager();
+
+            //referans tip
+            productManager.Add(product1);
+            Console.WriteLine(product1.ProductName);
+
+            //deger tip
+            //int sayi = 100;
+            //productManager.BirSeyYap(sayi);
+            //Console.WriteLine(sayi);
+
+            productManager.Topla2(3, 6);
+
+            int toplamSonucu = productManager.Topla(7,6);
+            Console.WriteLine(toplamSonucu);
+        }
+    }
+}
