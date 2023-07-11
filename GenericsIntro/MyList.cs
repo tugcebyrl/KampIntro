6573c57009b59effb16b232c6ef7eef48da9264d
Tugce BAYIRLI <tbayirli@hotmail.com>
2023-07-11 22:35:55 +0300

    GenericsIntro

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsIntro
+{
+    internal class MyList<T> // Class olusturduktan sonra <T> ekliyoruz
+    {
+        T[] items;
+        //Constructor ctor--> tab tab
+        public MyList() 
+        {
+            items = new T[0];
+        }
+        public void Add(T item) //verilen eleman turune gore listeye ekleme cikarma yapabiliyoruz
+        {
+            T[] tempArray = items; //Gecici dizi ile listedeki verilerin referans adresini sakliyoruz
+            items = new T[items.Length + 1];
+        }
+    }
+}
